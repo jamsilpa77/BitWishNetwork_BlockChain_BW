9c9c68881dd6954b91cdffb25a8e6eda2126f5b8
lds77diadem <lds77diadem@gmail.com>
2026-03-30 22:51:19 +0900

    2026.03.30: 비트위시 통합 경제/보안 분석 및 채굴 시스템 소스 코드 수정 완료

--- gen_hash.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-function generateHash(password, salt) {
-    return crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
-}
-
-const salt = crypto.randomBytes(16).toString('hex');
-const hash = generateHash('123456', salt);
-console.log(`${salt}:${hash}`);
